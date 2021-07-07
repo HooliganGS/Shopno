79e21de8004242484d76fe595ce71a9bd6da1c6f
HooliganGS <77269799+HooliganGS@users.noreply.github.com>
2021-07-07 14:36:51 +0300

    add: shopno files

---
+++ index.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const popupImages = document.querySelectorAll('.featured-img__style');
+    const popup = document.querySelector('.popup');
+
+    popupImages.forEach((img, index) => {
+        img.addEventListener('click', (img) => {
+            document.querySelector('.popup-img').src = img.target.src;
+        });
+        img.addEventListener('click', openPopup);
+    });
+
+    function openPopup() {
+        popup.classList.add('show-popup');
+        popup.classList.remove('hiden-popup');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closePopup() {
+        popup.classList.add('hiden-popup');
+        popup.classList.remove('show-popup');
+        document.body.style.overflow = '';
+    }
+
+    popup.addEventListener('click', (e) => {
+        if (e.target == popup) {
+            closePopup();
+        }
+    });
+});
